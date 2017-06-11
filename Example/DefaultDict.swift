d86fd9c5599fbfee690e0006ffbaacbf4c165eb5
Noah Gilmore <noah.w.gilmore@gmail.com>
2017-06-10 22:29:22 -0700

    Add more docs

--- Example/DefaultDict.swift
+++ Example/DefaultDict.swift
@@ -6,6 +6,13 @@
 //  Copyright © 2017 CocoaPods. All rights reserved.
 //
 
+/**
+ * A dictionary with default values.
+ *
+ * DefaultDict behaves in most respects like a regular dictionary, with the added fact that when a key
+ * that does not exist int he dictionary is accessed, a default value will be returned. A factory method
+ * for creating default values at access time can also be specified instead of a static value.
+ */
 public final class DefaultDict<Key: Hashable, Value>: CustomStringConvertible {
   typealias Factory = (Key) -> Value
   fileprivate var dict: Dictionary<Key, Value> = [:]
@@ -23,14 +30,31 @@ public final class DefaultDict<Key: Hashable, Value>: CustomStringConvertible {
     }
   }
 
+  /**
+   * Initialize the dict with a given default value.
+   *
+   * - Parameter value: The default value to return when a nonexistent key is accessed.
+   */
   init(value: Value) {
     self.factory = { _ in value }
   }
 
+  /**
+   * Initialize the dict with a factory which produces a default value based on the key which was accessed.
+   *
+   * - Parameter factory: A function which is called when a nonexistent key is accessed. This function
+   *   is passed the key which was accessed and its return value is reported as the default value. This
+   *   function will not be called on subsequent accesses of the same key (its return value will be saved).
+   */
   init(factory: @escaping Factory) {
     self.factory = factory
   }
 
+  /**
+   * Initialize the dict with a factory which produces a default value.
+   *
+   * - Parameter factory: A function which is called when a nonexistent key is accessed. See `init(factory:)`.
+   */
   init(factory: @escaping () -> Value) {
     self.factory = { _ in factory() }
   }
